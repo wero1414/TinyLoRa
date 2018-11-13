9cde9eaf90c501b4f105fbfba50b1c6f582d264e
brentru <brent@adafruit.com>
2018-11-13 11:35:45 -0500

    add in license text

--- TinyLoRa.h
+++ TinyLoRa.h
@@ -13,8 +13,21 @@
  * products from Adafruit!
  *
  * Copyright 2015, 2016 Ideetron B.V.
- * Modified by Brent Rubell for Adafruit Industries.
  *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Modified by Brent Rubell for Adafruit Industries.
  *
  * LGPL license, all text here must be included in any redistribution.
  *
